970937d76f641a057016fc199f45be33006c7e12
LAPTOP-7A7SKL1V\Sander van Delden <delden08@gmail.com>
2019-12-18 23:23:09 +0100

    fixed spelling error

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -5,31 +5,31 @@ const HomePage = () => (
     <div className='homepage'>
         <div className='directory-menu'>
             <div className='menu-item'>
-                <div className='çontent'>
+                <div className='content'>
                     <h1 className='title'>Hats</h1>
                     <span className='subtitle'>Shop now</span>
                 </div>
             </div>
             <div className='menu-item'>
-                <div className='çontent'>
+                <div className='content'>
                     <h1 className='title'>Jackets</h1>
                     <span className='subtitle'>Shop now</span>
                 </div>
             </div>
             <div className='menu-item'>
-                <div className='çontent'>
+                <div className='content'>
                     <h1 className='title'>Womens</h1>
                     <span className='subtitle'>Shop now</span>
                 </div>
             </div>
             <div className='menu-item'>
-                <div className='çontent'>
+                <div className='content'>
                     <h1 className='title'>Mens</h1>
                     <span className='subtitle'>Shop now</span>
                 </div>
             </div>
             <div className='menu-item'>
-                <div className='çontent'>
+                <div className='content'>
                     <h1 className='title'>Dogs</h1>
                     <span className='subtitle'>Shop now</span>
                 </div>
